213594e5bf968d8bb094f3769f4e3cfefbba92b8
rohitarora17 <rohit.arora@aroratechnologies.com>
2022-09-10 12:40:10 +0530

    Make it better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = 5000;
+const port = process.end.PORT || 5000;
 const cors = require("cors")
 var bodyParser = require('body-parser');
 app.use(cors())
